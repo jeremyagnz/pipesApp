48bdad8783772b6b2cfdf335b4ffec38aa5caae5
jeremyagnz <jeremyagnz@gmail.com>
2022-12-07 01:21:13 -0400

    Example OF pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesApp';
+
+  nombre: string = 'JeremY ArIas';
+
+  mostrarNombre(){
+    console.log(this.nombre);
+  }
 }
